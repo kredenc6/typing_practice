ee4569a8d26965ba21897d000c169dd69a53b7da
kredenc6 <weeve@email.cz>
2021-10-15 21:21:27 +0200

    charts optimized sizing

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,26 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, useLayoutEffect } from "react";
 import { Box, Paper, Typography } from "@material-ui/core";
+import SimpleBar from "simplebar-react";
 import ReactApexCharts from "react-apexcharts";
 import { LOCAL_STORAGE_KEYS } from "../../constants/constants";
 import { MistypedWordsLog, Results } from "../../types/otherTypes";
-import defaultLastResultsChartOptions, { mistypedWordsChartDefaultOptions, timestampsToXAxisCategories, updateXAxisCategories } from "./lastResultsChartOptions";
+import {
+  defaultLastResultsChartOptions, mistypedWordsChartDefaultOptions,
+  timestampsToXAxisCategories
+} from "./chartOptions";
 import { makeStyles } from "@material-ui/styles";
 import { sortMistypedWords } from "./helpFunction";
+import "simplebar/dist/simplebar.min.css";
+
+const LAST_RESULTS_CHART_MINIMAL_WIDTH = 500;
+const MISTYPE_WORDS_CHART_MINIMAL_HEIGHT = 450;
 
 const useStyles = makeStyles({
   lastResultsChartWrapper: {
-    maxWidth: "90vw",
+    display: "grid",
+    gridTemplateColumns: "1fr",
+    justifyItems: "center",
+    minWidth: `${LAST_RESULTS_CHART_MINIMAL_WIDTH}px`,
+    width: "90vw",
     margin: "1rem auto",
-    width: ({ itemCount }: { itemCount: number; }) => `${Math.max(500, itemCount * 120)}px`,
-    height: "20rem",
+    paddingBottom: "1.5rem"
+    // width: ({ itemCount }: { itemCount: number; }) => `${Math.max(LAST_RESULTS_CHART_MINIMAL_WIDTH, itemCount * 120)}px`
   },
    mistypedWordsWrapper: {
-     maxHeight: "50vh",
+     maxHeight: "90%",
+     height: "calc(48vh - 2rem)",
      width: "95%",
-     margin: "1rem auto",
-     padding: "1rem",
-     overflowX: "hidden",
-     overflowY: "auto"
+     margin: "0 auto",
+     padding: "5px 0 7px"
    }
 });
 
@@ -30,8 +41,13 @@ export default function Statistics() {
   const [textLength, setTextLength] = useState<number[]>([]);
   const [lastResultsChartOptions, setLastResultsChartOptions] =
     useState(defaultLastResultsChartOptions);
-  const [sumOfMistypes, setSumOfMistypes] = useState<{x: string; y: number; }[]>([]);
-  const classes = useStyles({ itemCount: typingSpeed.length});
+  const [mistypedWordsSeries, setMistypedWordsSeries] = useState<{x: string; y: number; }[]>([]);
+  const [mistypedWordsChartHeight, setMistypedWordsChartHeight] = useState(MISTYPE_WORDS_CHART_MINIMAL_HEIGHT);
+  const [simpleBarheight, setSimpleBarHeight] = useState("0px");
+  const classes = useStyles({ itemCount: typingSpeed.length });
+
+  const mistypedWordsWrapperRef = useRef<null | HTMLDivElement>(null);
+  const chartWidth = `${Math.max(LAST_RESULTS_CHART_MINIMAL_WIDTH, typingSpeed.length * 120)}px`;
   
   useEffect(() => {
     const lastResultsFromStorage = localStorage.getItem(LOCAL_STORAGE_KEYS.LAST_RESULTS);
@@ -57,34 +73,52 @@ export default function Statistics() {
       setTextLength(savedTextLength);
       const xAxisCategories = timestampsToXAxisCategories(savedResultTimestamps);
       
-      setLastResultsChartOptions( prev => {
-        return updateXAxisCategories(prev, xAxisCategories);
-      });
+      setLastResultsChartOptions({ xaxis: { categories: xAxisCategories }});
     } else {
       // TODO this won't be needed if the chart won't be rendered on falsy value
       setPrecision(savedPrecision);
       setTypingSpeed(savedTypingSpeed);
       setTextLength(savedTextLength);
       
-      setLastResultsChartOptions( prev => {
-        return updateXAxisCategories(prev, []);
-      });
+      setLastResultsChartOptions({ xaxis: { categories: [] }});
     }
 
     if(mistypedWordsFromStorage) {
       const savedMistypedWords = JSON.parse(mistypedWordsFromStorage) as MistypedWordsLog;
       const sortedMistypedWordsKeyValue = sortMistypedWords(savedMistypedWords);
       
+      let mistypesCount = 0;
       const mistypedWordsSeries = sortedMistypedWordsKeyValue
-        .map(([key, { sumOfMistypes }]) => ({ x: key, y: sumOfMistypes }));
-      setSumOfMistypes(mistypedWordsSeries);
+        .map(([key, { sumOfMistypes }]) => {
+          mistypesCount += sumOfMistypes;  
+          return { x: key, y: sumOfMistypes };
+        });
+      setMistypedWordsSeries(mistypedWordsSeries);
+      setMistypedWordsChartHeight( Math.max(MISTYPE_WORDS_CHART_MINIMAL_HEIGHT, mistypesCount * 3));
     }
   }, [])
 
+  useLayoutEffect(() => {
+    const setSimpleBarHeightFun = () => {
+      if(mistypedWordsWrapperRef.current) {
+        const {
+          height,
+          paddingTop,
+          paddingBottom
+        } = getComputedStyle(mistypedWordsWrapperRef.current);
+        setSimpleBarHeight(`calc(${height} - ${paddingTop} - ${paddingBottom})`);
+      }
+    };
+    
+    setSimpleBarHeightFun();
+    window.addEventListener("resize", setSimpleBarHeightFun);
+    return () => window.removeEventListener("resize", setSimpleBarHeightFun);
+  },[])
+
   return (
-    <Box>
-      <Typography variant="h4" align="center">Statistics</Typography>
+    <Box sx={{ height: "100vh", display: "grid", gridTemplateColumns: "1fr", gridTemplateRows: "50% 50%", textAlign: "center" }}>
       <Box className={classes.lastResultsChartWrapper}>
+        <Typography variant="h6" align="center">Výsledky za posledních 10 opisů</Typography>
         <ReactApexCharts
           id="lastResultsChart"
           options={lastResultsChartOptions}
@@ -93,19 +127,29 @@ export default function Statistics() {
             { name: "rychlost", data: typingSpeed, type: "bar" },
             { name: "délka textu", data: textLength, type: "line" }
           ]}
-          // type="bar"
-          width="100%"
+          width={chartWidth}
           height="100%" />
       </Box>
-      <Paper className={classes.mistypedWordsWrapper}>
-        <ReactApexCharts
-          id="mistypedWordsChart"
-          options={mistypedWordsChartDefaultOptions}
-          series={[{ name: "slova", data: sumOfMistypes }]}
-          type="treemap"
-          width="100%"
-          height="200" />
-      </Paper>
+      <Box>
+        <Typography variant="h6" align="center">Chybně napsaná slova</Typography>
+        <Paper
+          variant="outlined"
+          className={classes.mistypedWordsWrapper}
+          {...{ ref: mistypedWordsWrapperRef } as any}
+        >
+          <SimpleBar
+            style={{ overflowX: "hidden", padding: "0 10px", maxHeight: simpleBarheight }}
+          >
+            <ReactApexCharts
+              id="mistypedWordsChart"
+              options={mistypedWordsChartDefaultOptions}
+              series={[{ name: "slova", data: mistypedWordsSeries }]}
+              type="treemap"
+              width="100%"
+              height={`${mistypedWordsChartHeight}px`} />
+          </SimpleBar>
+        </Paper>
+      </Box>
     </Box>
   );
 }
--- src/components/Statistics/chartOptions.ts
+++ src/components/Statistics/chartOptions.ts
@@ -1,8 +1,8 @@
 import { ApexOptions } from "apexcharts";
 import dateFormat from "dateformat";
 
-const defaultLastResultsChartOptions: ApexOptions = {
-  colors: ["rgba(0, 143, 251, 0.9)", "rgba(91, 209, 215, 0.9)", "rgba(240, 91, 76, 0.9)"],
+export const defaultLastResultsChartOptions: ApexOptions = {
+  colors: ["rgba(0, 143, 251, 0.9)", "rgba(0, 227, 150, 0.9)", "rgba(254, 176, 25, 0.9)"],
   chart: {
     id: "lastResultsChart",
     toolbar: {
@@ -12,9 +12,20 @@ const defaultLastResultsChartOptions: ApexOptions = {
       enabled: false
     }
   },
+  markers: {
+    size: 5
+  },
+  legend: {
+    // offsetY: -8
+  },
   stroke: {
-    colors: ["rgba(0, 143, 251, 0)", "rgba(91, 209, 215, 0)", "rgba(240, 91, 76, 0.9)"],
-    lineCap: "round"
+    colors: ["rgba(0, 143, 251, 0.9)", "rgba(0, 227, 150, 0.9)", "rgba(254, 176, 25, 0.9)"],
+    curve: "smooth",
+    lineCap: "round",
+    width: 2
+  },
+  dataLabels: {
+    enabled: false
   },
   tooltip: {
     y: {
@@ -25,12 +36,18 @@ const defaultLastResultsChartOptions: ApexOptions = {
     }
   },
   xaxis: {
+    crosshairs: {
+      show: false
+    },
     tooltip: {
       enabled: false
     }
   },
   yaxis: [
     {
+      crosshairs: {
+        show: false
+      },
       title: {
         text: "přesnost"
       },
@@ -39,6 +56,9 @@ const defaultLastResultsChartOptions: ApexOptions = {
       }
     },
     {
+      crosshairs: {
+        show: false
+      },
       opposite: true,
       title: {
         text: "rychlost (úhozy za minutu)"
@@ -48,6 +68,9 @@ const defaultLastResultsChartOptions: ApexOptions = {
       }
     },
     {
+      crosshairs: {
+        show: false
+      },
       show: false
     }
   ]
@@ -55,25 +78,25 @@ const defaultLastResultsChartOptions: ApexOptions = {
 
 export const mistypedWordsChartDefaultOptions: ApexOptions = {
   chart: {
+    offsetY: -3,
     id: "mistypedWordsChart",
     toolbar: {
       show: false
     }
   },
+  plotOptions: {
+    treemap: {
+      shadeIntensity: 0.3,
+      useFillColorAsStroke: true
+    }
+  },
+  tooltip: {
+    marker: {
+      show: false
+    }
+  }
 };
 
 export const timestampsToXAxisCategories = (timestamps: number[]) => {
   return timestamps.map(timestamp => dateFormat(timestamp, "d. m. HH:MM"));
 };
-
-export const updateXAxisCategories = (previousOptions : ApexOptions, categories: string[]): ApexOptions => {
-  // TODO use lodash for general update since I have it?
-  return {
-    ...previousOptions,
-    xaxis: {
-      ...previousOptions.xaxis,
-      categories
-  }};
-};
-
-export default defaultLastResultsChartOptions;
