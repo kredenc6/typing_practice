d5dfffac7da7fcdd12ae583a45025aca994aa352
Filip Srankota <filip.srankota@seznam.cz>
2022-08-01 22:08:34 +0200

    LoadedParagraph tooltip fixed

--- src/components/LoadedParagraph/LoadedParagraph.tsx
+++ src/components/LoadedParagraph/LoadedParagraph.tsx
@@ -35,14 +35,6 @@ const useStyles = makeStyles({
     "& .MuiTooltip-tooltip": {
       maxWidth: "20rem",
       width: "20rem"
-    },
-    "& .MuiTooltip-arrow": {
-      backgroundColor: "transparent",
-      width: 0,
-      height: 0,
-      border: "6px solid transparent",
-      borderTopColor: "inherit",
-      transform: "translateY(35%)"
     }
   }
 });
@@ -70,11 +62,11 @@ export default function LoadedParagraph({
           horizontal: "left",
         }}
       >
-        <Typography className={classes.paragraphId}>{paragraphId})</Typography>
+        <Typography className={classes.paragraphId}>{paragraphId}</Typography>
           <Tooltip
             title={
               <Simplebar
-                style={{ paddingRight: "10px", maxHeight: "200px", width: "20rem" }}
+                style={{ paddingRight: "0.6rem", maxHeight: "200px", width: "20rem" }}
                 autoHide={false}
               >
                   {loadedParagraph}
@@ -83,6 +75,7 @@ export default function LoadedParagraph({
             arrow
             enterDelay={500}
             enterNextDelay={500}
+            leaveDelay={200}
             placement="top"
             aria-label={`paragraph ${paragraphId}`}
             PopperProps={{ className: classes.tooltip }}
