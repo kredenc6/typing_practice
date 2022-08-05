72fe6950b398ab599f45077e8a39880e8bef3d4f
Filip Srankota <filip.srankota@seznam.cz>
2022-08-05 16:07:38 +0200

    SelectTextDislpayThemeButton fixed

--- src/components/TextDisplayThemeSelector/SelectTextDisplayThemeButton/SelectTextDisplayThemeButton.tsx
+++ src/components/TextDisplayThemeSelector/SelectTextDisplayThemeButton/SelectTextDisplayThemeButton.tsx
@@ -1,8 +1,8 @@
-import { Button, ButtonProps } from "@mui/material";
-import { makeStyles } from "@mui/styles";
+import { Button, ButtonProps, CSSObject, Typography } from "@mui/material";
 import { useContext } from "react";
 import { PlayPageThemeContext } from "../../../styles/themeContexts";
 import { TextDisplayTheme } from "../../../types/themeTypes";
+import { SymbolCorrectness } from "../../../types/symbolTypes";
 
 const BUTTON_TEXT = "barevné téma";
 
@@ -10,62 +10,46 @@ interface Props extends ButtonProps {
   themeToSelect: Omit<TextDisplayTheme, "offset">;
 }
 
-interface UseStylesProps {
-  themeToSelect: Props["themeToSelect"];
-  textDisplayTheme: TextDisplayTheme;
-}
-
-const useStyles = makeStyles(({ palette }) => ({
-  paletteButton: {
-    backgroundColor: ({ themeToSelect }: UseStylesProps) => themeToSelect.background.main,
-    border: ({ textDisplayTheme }: UseStylesProps) => `1px solid ${textDisplayTheme.text.secondary}`,
-    "&.Mui-disabled": {
-      border: `2px solid ${palette.info.dark}`
-    },
-    "&:hover": {
-      backgroundColor: palette.info.dark,
-      border: `1px solid ${palette.info.dark}`
-    }
-  },
-  correct: {
-    color: ({ themeToSelect }: UseStylesProps) => themeToSelect.symbols.correct.color,
-    backgroundColor: ({ themeToSelect }: UseStylesProps) => themeToSelect.symbols.correct.backgroundColor
-  },
-  corrected: {
-    color: ({ themeToSelect }: UseStylesProps) => themeToSelect.symbols.corrected.color,
-    backgroundColor: ({ themeToSelect }: UseStylesProps) => themeToSelect.symbols.corrected.backgroundColor
-  },
-  mistyped: {
-    color: ({ themeToSelect }: UseStylesProps) => themeToSelect.symbols.mistyped.color,
-    backgroundColor: ({ themeToSelect }: UseStylesProps) => themeToSelect.symbols.mistyped.backgroundColor
-  },
-  symbol: {
-    marginRight: "1px",
-    padding: "0 1px",
-    borderRadius: "2px"
-  }
-}));
-
-export default function SelectTextDisplayThemeButton(
-  { themeToSelect: availableTextDisplayTheme, ...buttonProps }: Props)
-{
+export default function SelectTextDisplayThemeButton({
+  themeToSelect, ...buttonProps
+}: Props) {
   const { state: textDisplayTheme } = useContext(PlayPageThemeContext);
-  const classes = useStyles({ themeToSelect: availableTextDisplayTheme, textDisplayTheme });
 
-  const ButtonSpanComponents = BUTTON_TEXT
+  const ButtonLetterComponents = BUTTON_TEXT
     .split("")
     .map((letter, i) => {
-      let classNames = `${classes.symbol} `;
-      if(i === 2)      classNames += classes.corrected;
-      else if(i === 9) classNames += classes.mistyped;
-      else             classNames += classes.correct;
+      const createLetterStyle = (correctness: Exclude<SymbolCorrectness, "invalid" | "pending">): CSSObject => ({
+        marginRight: "1px",
+        padding: "0 1px",
+        fontSize: "0.9rem",
+        borderRadius: "2px",
+        color: themeToSelect.symbols[correctness as keyof typeof themeToSelect.symbols].color,
+        backgroundColor: themeToSelect.symbols[correctness as keyof typeof themeToSelect.symbols].backgroundColor
+      });
+
+      let letterStyle: CSSObject;
+      if(i === 2)      letterStyle = createLetterStyle("corrected");
+      else if(i === 9) letterStyle = createLetterStyle("mistyped");
+      else             letterStyle = createLetterStyle("correct");
       
-      return <span key={i} className={classNames}>{letter}</span>;
+      return <Typography key={i} sx={letterStyle}>{letter}</Typography>;
   });
 
   return (
-    <Button className={classes.paletteButton} {...buttonProps}>
-      {ButtonSpanComponents}
+    <Button
+      sx={({ palette }) => ({
+        background: themeToSelect.background.main,
+        border: `1px solid ${textDisplayTheme.text.secondary}`,
+        "&.Mui-disabled": {
+          border: `2px solid ${palette.info.dark}`
+        },
+        "&:hover": {
+          backgroundColor: palette.info.dark,
+          border: `1px solid ${palette.info.dark}`
+        }})
+      }
+      {...buttonProps}>
+      {ButtonLetterComponents}
     </Button>
   );
 }
