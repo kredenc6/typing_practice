71116ad6641bce52a7857306ebc8f3cf6ca1f96a
Filip Srankota <filip.srankota@seznam.cz>
2022-08-09 11:56:05 +0200

    PlaySettingsPopper placement and border fixed

--- src/components/PlaySettings/PlaySettingsPopper/PlaySettingsPopper.tsx
+++ src/components/PlaySettings/PlaySettingsPopper/PlaySettingsPopper.tsx
@@ -1,33 +1,27 @@
 import { useContext } from "react";
 import { Popper, PopperProps } from "@mui/material";
-import { makeStyles } from "@mui/styles";
 import { PlayPageThemeContext } from "../../../styles/themeContexts";
-import { TextDisplayTheme } from "../../../types/themeTypes";
-
-const useStyles = makeStyles({
-  popper: {
-    width: "23rem",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    marginTop: "-1px",
-    padding: "0.5rem",
-    backgroundColor: (textDisplayTheme: TextDisplayTheme) => textDisplayTheme.background.secondary,
-    borderTop: (textDisplayTheme: TextDisplayTheme) => `1px solid ${textDisplayTheme.background.secondary}`,
-    borderRight: (textDisplayTheme: TextDisplayTheme) => `1px solid ${textDisplayTheme.text.secondary}`,
-    borderBottom: (textDisplayTheme: TextDisplayTheme) => `1px solid ${textDisplayTheme.text.secondary}`,
-    borderLeft: (textDisplayTheme: TextDisplayTheme) => `1px solid ${textDisplayTheme.text.secondary}`,
-    zIndex: 3
-  }
-});
 
 export default function PlaySettingPopper({ children, ...popperProps }: PopperProps) {
   const { state: textDisplayTheme } = useContext(PlayPageThemeContext);
-  const classes = useStyles(textDisplayTheme);
 
   return (
     <Popper
-      className={classes.popper}
+      sx={{
+        width: "23rem",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: "-1px !important", // important overrides the placement prop
+        marginRight: "2rem !important",
+        padding: "0.5rem",
+        backgroundColor: textDisplayTheme.background.secondary,
+        borderTop: `1px solid ${textDisplayTheme.background.secondary}`,
+        borderRight: `1px solid ${textDisplayTheme.text.secondary}`,
+        borderBottom: `1px solid ${textDisplayTheme.text.secondary}`,
+        borderLeft: `1px solid ${textDisplayTheme.text.secondary}`,
+        zIndex: 3
+      }}
       id="playSettingsPopper"
       placement="bottom-end"
       {...popperProps}
