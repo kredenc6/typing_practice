9f9731459e2ad78cd0a975356bf5d1a8049cba76
Filip Srankota <filip.srankota@seznam.cz>
2022-08-11 16:41:22 +0200

    InvalidSymbolsMessage switched to emotion

--- src/components/InvalidSymbolsMessage/InvalidSymbolsMessage.tsx
+++ src/components/InvalidSymbolsMessage/InvalidSymbolsMessage.tsx
@@ -1,21 +1,12 @@
 import { Box, Typography } from "@mui/material";
-import { makeStyles } from "@mui/styles";
 
 interface Props {
   invalidSymbols: string[];
 }
 
-const useStyles = makeStyles({
-  invalidSymbolsText: {
-    textAlign: "center"
-  }
-});
-
 export default function InvalidSymbolsMessage({ invalidSymbols }: Props) {
-  const classes = useStyles();
-
   return (
-    <Box className={classes.invalidSymbolsText}>
+    <Box sx={{ textAlign: "center" }}>
       {!!invalidSymbols.length &&
         <Typography>
           Text k opsání obsahuje tyto neplatné symboly, které nebudou v opisu zobrazeny: {invalidSymbols.join(", ")}
