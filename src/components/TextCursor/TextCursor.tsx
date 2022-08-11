40e7085d2c166e07e0a63be1a4d2791787db7505
Filip Srankota <filip.srankota@seznam.cz>
2022-08-11 16:48:05 +0200

    TextCursor swithced to emotion

--- src/components/TextCursor/TextCursor.tsx
+++ src/components/TextCursor/TextCursor.tsx
@@ -1,27 +1,20 @@
 import { Box } from "@mui/material";
-import { makeStyles } from "@mui/styles";
 
 interface Props {
   height: string;
 }
 
-const useStyles = makeStyles(({ palette }) => ({
-  textCursor: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    borderBottomWidth: ({ height }: Props) => height,
-    borderBottomStyle: "solid",
-    borderBottomColor: `${palette.info.main}`
-  }
-}));
-
 export default function TextCursor({ height }: Props) {
-  const classes = useStyles({ height });
-
   return (
-    <Box className={classes.textCursor}></Box>
+    <Box sx={({ palette }) => ({
+      position: "absolute",
+      bottom: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderBottomWidth: height,
+      borderBottomStyle: "solid",
+      borderBottomColor: `${palette.info.main}`
+    })} />
   );
 }
