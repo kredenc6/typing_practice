18549caa8b328d7c2b9733b53c04a4a0267e432a
Filip Srankota <filip.srankota@seznam.cz>
2022-08-11 19:43:39 +0200

    got rid of DefaultTheme

--- src/styles/appTheme.ts
+++ src/styles/appTheme.ts
@@ -1,10 +1,5 @@
 import { createTheme, PaletteMode, ThemeOptions } from "@mui/material";
-import { Theme } from '@mui/material/styles';
 import { LOCAL_STORAGE_KEYS } from "../constants/constants";
-
-declare module "@mui/styles/defaultTheme" {
-  interface DefaultTheme extends Theme {}
-}
 
 const darkThemeOptions: ThemeOptions = {
   palette: {
