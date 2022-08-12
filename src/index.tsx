195f05e1fa316023dd33ae0a086cd84c503bf198
Filip Srankota <filip.srankota@seznam.cz>
2022-08-12 17:27:59 +0200

    enabled strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/global.css";
 import App from "./App";
@@ -7,13 +6,11 @@ import * as serviceWorker from "./serviceWorker";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
-root.render(<App />);
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
